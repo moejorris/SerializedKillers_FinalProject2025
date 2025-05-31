e38fc4ccc2417f8439f888f89b4cfdfeb163edea
moejorris <136379332+moejorris@users.noreply.github.com>
2025-05-31 13:44:17 -0400

    Update RootMotion_Translator.cs

--- Assets/Scripts/General/RootMotion_Translator.cs
+++ Assets/Scripts/General/RootMotion_Translator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class RootMotion_Translator : MonoBehaviour
 {
     //Translates the motion used in a root motion animation to readable values that can be applied to Rigidbodies, Character Controllers, and Transforms other than what is moved in the Animation. Developed for use with the Player Character's Attack Animations.
-    [SerializeField] Animator animator;
+    Animator animator;
 
     public Vector3 RootMovement { get => _moveForce; }
     public Quaternion RootRotation { get => _rotation; }
