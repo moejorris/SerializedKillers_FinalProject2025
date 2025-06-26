d253767701200e7d7beacfb547fb6bc9c5ae80d9
moejorris <136379332+moejorris@users.noreply.github.com>
2025-06-25 23:42:59 -0400

    TEST - Update IDamageable.cs to include optional inclusion of script steal, as well as ignoring parameter altogether
    -No params, damage, script steal, or both

--- Assets/Scripts/Player/PlayerCombat/IDamageable.cs
+++ Assets/Scripts/Player/PlayerCombat/IDamageable.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public interface IDamageable
 {
+    public void TakeDamage();
     public void TakeDamage(float damage);
+    public void TakeDamage(Player_ScriptSteal scriptSteal);
+    public void TakeDamage(float damage, Player_ScriptSteal scriptSteal);
 }
