6c09cbeaf2ea054633c6a59c3781ff6e049b8d6f
SkullyWasTaken <159203417+SkullyWasTaken@users.noreply.github.com>
2025-07-07 02:06:28 -0400

    Rope Cut Fix

--- Assets/Scripts/Puzzles/CutRope.cs
+++ Assets/Scripts/Puzzles/CutRope.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class CutRope : MonoBehaviour, IDamageable
+public class CutRope : MonoBehaviour, IElemental
 {
     private Animator anim;
     [Header("Puzzle Settings")]
     [Tooltip("The name of the behavior required to cut the rope.")]
-    [SerializeField] private Behavior heldBehavior;
+    [SerializeField] private string requiredBehaviorName;
     [SerializeField] private GameObject gateToOpen;
 
     void Start()
@@ -13,20 +13,40 @@ public class CutRope : MonoBehaviour, IDamageable
         anim = GetComponent<Animator>();
     }
 
-    public void TakeDamage(float damage = 0)
+    // public void TakeDamage(float damage = 0)
+    // {
+    //     if (PlayerController.instance.ScriptSteal.GetHeldBehavior() != null && PlayerController.instance.ScriptSteal.GetHeldBehavior() == heldBehavior && PlayerController.instance.ScriptSteal.BehaviorActive())
+    //     {
+    //         anim.SetTrigger("Cut"); // Trigger the cut animation
+    //         Debug.Log("Rope cut with behavior: " + heldBehavior.name);
+    //         Collider col = GetComponent<Collider>();
+    //         if (col != null) col.enabled = false;
+    //     }
+    //     else
+    //     {
+    //         string heldBehaviorName = "none";
+    //         if (PlayerController.instance.ScriptSteal.GetHeldBehavior() != null) heldBehaviorName = PlayerController.instance.ScriptSteal.GetHeldBehavior().behaviorName;
+    //         Debug.Log("Rope cutting failed. Required behavior: " + heldBehavior.name + ", but held behavior: " + heldBehaviorName);
+    //     }
+    // }
+
+    public void InteractElement(Behavior behavior)
     {
-        if (PlayerController.instance.ScriptSteal.GetHeldBehavior() != null && PlayerController.instance.ScriptSteal.GetHeldBehavior() == heldBehavior && PlayerController.instance.ScriptSteal.BehaviorActive())
+        if (behavior != null)
         {
-            anim.SetTrigger("Cut"); // Trigger the cut animation
-            Debug.Log("Rope cut with behavior: " + heldBehavior.name);
-            Collider col = GetComponent<Collider>();
-            if (col != null) col.enabled = false;
-        }
-        else
-        {
-            string heldBehaviorName = "none";
-            if (PlayerController.instance.ScriptSteal.GetHeldBehavior() != null) heldBehaviorName = PlayerController.instance.ScriptSteal.GetHeldBehavior().behaviorName;
-            Debug.Log("Rope cutting failed. Required behavior: " + heldBehavior.name + ", but held behavior: " + heldBehaviorName);
+            if (behavior.behaviorName.ToLower() == requiredBehaviorName.ToLower())
+            {
+                anim.SetTrigger("Cut"); // Trigger the cut animation
+                Debug.Log("Rope cut with behavior: " + behavior.behaviorName);
+                Collider col = GetComponent<Collider>();
+                if (col != null) col.enabled = false;
+            }
+            else
+            {
+                string heldBehaviorName = "none";
+                if (PlayerController.instance.ScriptSteal.GetHeldBehavior() != null) heldBehaviorName = PlayerController.instance.ScriptSteal.GetHeldBehavior().behaviorName;
+                Debug.Log("Rope cutting failed. Required behavior: " + requiredBehaviorName + ", but held behavior: " + heldBehaviorName);
+            }
         }
     }
 
